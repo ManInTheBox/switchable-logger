b6b92020ae23bcbc90de3af4d4ea88bc512cb90d
Zarko Stankovic <stankovic.zarko@gmail.com>
2019-04-12 01:54:49 +0200

    Better const names

--- src/App/Infrastructure/Logging/TimeBasedLoggingState.php
+++ src/App/Infrastructure/Logging/TimeBasedLoggingState.php
@@ -6,11 +6,11 @@ namespace App\Infrastructure\Logging;
 
 class TimeBasedLoggingState implements LoggingStateInterface
 {
-    private const WAKE = 7;
-    private const SLEEP = 21;
+    private const START = 21;
+    private const END = 7;
 
     public function isEnabled(): bool
     {
-        return date('H') > self::SLEEP || date('H') < self::WAKE;
+        return date('H') > self::START || date('H') < self::END;
     }
 }
